97d6f214b5d835c781757f78c7414fae0cdc5811
Martina <martina.gabrielli@yahoo.it>
2025-04-10 22:56:01 +0100

    add tailwind variables

--- app/components/SideBar/SideBar.tsx
+++ app/components/SideBar/SideBar.tsx
@@ -9,14 +9,14 @@ interface SideBarProps {
 export default function SideBar({onClick, menu}: SideBarProps) {
     return ( 
         <div className={`sidebar absolute top-0 transition-all ease-in ${menu}`}>
-            <div className="bg-white relative z-100 border-2 border-gray-300 rounded-lg p-4 h-screen w-100">
+            <div className="bg-background relative z-100 border-2 border-gray-300 rounded-lg p-4 h-screen w-100">
                 <div className="flex flex-col h-16 w-full">
                     <div className="flex justify-between w-full">
                         <BurgerMenu onClick={onClick} />
                         <Logo />
                     </div>
-                    <div className="flex flex-col justify-start mt-5 px-4">
-                        <ul className="d-flex space-y-2">
+                    <div className="flex flex-col justify-start mt-10 px-4">
+                        <ul className="d-flex space-y-2 body-1">
                             <li>Menu item 1</li>
                             <li>Menu item 2</li>
                             <li>Menu item 3</li>
