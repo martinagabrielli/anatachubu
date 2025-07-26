9a457e1c9c4f41f77443041768f4d00146f1aae5
Martina <martina.gabrielli@yahoo.it>
2025-07-26 21:00:31 +0100

    styling updates

--- app/components/Spinner/Spinner.tsx
+++ app/components/Spinner/Spinner.tsx
@@ -1,5 +1,5 @@
 export default function Spinner() {
     return (
-       <div className="size-10 mx-auto border-3 border-t-transparent border-black rounded-full animate-spin"></div>
+       <div className="size-10 mx-auto border-3 border-t-transparent border-foreground rounded-full animate-spin"></div>
     );
 }
